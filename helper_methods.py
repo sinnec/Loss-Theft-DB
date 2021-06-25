49ccf5c11ce14fc5f3dbd9a0031ffe45b43c2d0e
John Zaras <john.zaras@yahoo.com>
2021-06-25 21:59:08 +0300

    -new final s checher function added\n-Minor variable name changes

--- helper_methods.py
+++ helper_methods.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
 def datetime_formatter(timestring, from_time, to_time):
-    protocol_date_obj = datetime.strptime(timestring, from_time)
-    return protocol_date_obj.strftime(to_time)
+    date_obj = datetime.strptime(timestring, from_time)
+    return date_obj.strftime(to_time)
 
 def latin_to_greek(name):
     eng = 'abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ'
@@ -15,13 +15,13 @@ def latin_to_greek(name):
             new_name += i
     return new_name
 
-def greek_accent_remover(string_name):
+def greek_accent_remover(name):
     puncts = {'ά': 'α', 'έ': 'ε', 'ή': 'η', 'ί': 'ι', 'ΐ': 'ϊ', 'ό': 'ο', 'ύ': 'υ', 'ώ': 'ω',
                 'Ά': 'Α', 'Έ': 'Ε', 'Ή': 'Η', 'Ί': 'Ι', 'Ό': 'Ο', 'Ύ': 'Υ', 'Ώ': 'Ω'}
     dieresis = {'ι': 'ϊ', 'υ': 'ϋ'}
     new_name = ''
     prev_char = 0
-    for c in string_name:
+    for c in name:
         char = c
         if c in puncts.keys():
             char = puncts[c]
@@ -30,6 +30,16 @@ def greek_accent_remover(string_name):
         prev_char = c
         new_name += char
     return new_name
+
+def final_s_checker(name):
+    new_name = ""
+    for i, letter in enumerate(name):
+        if letter == "σ" and (i==len(name) - 1 or name[i+1] == " " or name[i+1] == "-"):
+            new_name += "ς"
+        else:
+            new_name += letter
+    return new_name
+
 
 def window_centre_position(window):
         window_width = window.winfo_reqwidth()
